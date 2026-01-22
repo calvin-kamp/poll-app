7f8d8a5dc25ac1de5751582327291f06c1e4c6cc
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-01-23 00:45:41 +0100

    feat: add button component

---
+++ src/app/shared/ui/button/button.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ui-button',
+    imports: [],
+    templateUrl: './button.html',
+    styleUrl: './button.scss',
+})
+
+export class Button {
+
+}
