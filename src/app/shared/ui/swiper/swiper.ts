b1d7ebc2c4d1309445721676ea1c272dab619a72
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-01-23 00:45:18 +0100

    feat: add swiper component

---
+++ src/app/shared/ui/swiper/swiper.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ui-swiper',
+    imports: [],
+    templateUrl: './swiper.html',
+    styleUrl: './swiper.scss',
+})
+
+export class Swiper {
+
+}
