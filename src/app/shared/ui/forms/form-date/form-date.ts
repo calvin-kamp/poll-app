ce6a847aaea1af9dd4ff4126e656bfd1dda685c3
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-01-23 00:46:06 +0100

    feat: add form-date component

---
+++ src/app/shared/ui/forms/form-date/form-date.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-form-date',
+    imports: [],
+    templateUrl: './form-date.html',
+    styleUrl: './form-date.scss',
+})
+export class FormDate {
+
+}
