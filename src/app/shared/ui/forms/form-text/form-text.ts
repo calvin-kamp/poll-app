655776e054e76f902ff1af478974badaa1a1611f
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-01-23 00:46:31 +0100

    feat: add form-text component

---
+++ src/app/shared/ui/forms/form-text/form-text.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-form-text',
+    imports: [],
+    templateUrl: './form-text.html',
+    styleUrl: './form-text.scss',
+})
+export class FormText {
+
+}
