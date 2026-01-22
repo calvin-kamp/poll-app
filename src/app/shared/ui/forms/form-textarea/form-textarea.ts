c78d16223ad794524f7783d24209c7cf1620104b
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-01-23 00:46:45 +0100

    feat: add form-textarea component

---
+++ src/app/shared/ui/forms/form-textarea/form-textarea.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-form-textarea',
+    imports: [],
+    templateUrl: './form-textarea.html',
+    styleUrl: './form-textarea.scss',
+})
+export class FormTextarea {
+
+}
