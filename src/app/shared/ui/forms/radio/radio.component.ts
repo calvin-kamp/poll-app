feb0a14f1e5d8b66a0b4e3415905e4e256c9f5ec
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-01-23 16:28:47 +0100

    feat: update radio-component

--- src/app/shared/ui/forms/radio/radio.component.ts
+++ src/app/shared/ui/forms/radio/radio.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'ui-radio',
-    imports: [],
+    imports: [ReactiveFormsModule],
     templateUrl: './radio.component.html',
     styleUrl: './radio.component.scss',
 })
 
 export class RadioComponent {
+    label = input.required<string>();
+    name = input.required<string>();
+    value = input.required<string>();
+    errorMessage = input<string>('');
 
+    control = input<FormControl<string | null>>(new FormControl<string | null>(null));
 }
