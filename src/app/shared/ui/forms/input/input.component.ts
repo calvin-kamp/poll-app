1176ff6103ce4a17f12407e488f9f993194c5230
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-01-23 16:28:39 +0100

    feat: update input-component

--- src/app/shared/ui/forms/input/input.component.ts
+++ src/app/shared/ui/forms/input/input.component.ts
@@ -1,15 +1,46 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, effect, input, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'ui-input',
-    imports: [],
+    standalone: true,
+    imports: [ReactiveFormsModule],
     templateUrl: './input.component.html',
     styleUrl: './input.component.scss',
 })
 
 export class InputComponent {
-    label = input<string>('');
+    label = input.required<string>();
+    name = input.required<string>();
     type = input<'text' | 'password' | 'email' | 'number'>('text');
-    placeholder = input<string>('');
     errorMessage = input<string>('');
+
+    control = input<FormControl<string | null>>(new FormControl<string | null>(null));
+
+    private isFocused = signal(false);
+    private hasValue = signal(false);
+
+    isLabelFloated = computed(() => this.isFocused() || this.hasValue());
+
+    constructor() {
+        effect((onCleanup) => {
+            const c = this.control();
+
+            this.hasValue.set(((c.value ?? '').trim().length > 0));
+
+            const sub = c.valueChanges.subscribe((v) => {
+                this.hasValue.set(((v ?? '').trim().length > 0));
+            });
+
+            onCleanup(() => sub.unsubscribe());
+        });
+    }
+
+    onFocusIn() {
+        this.isFocused.set(true);
+    }
+
+    onFocusOut() {
+        this.isFocused.set(false);
+    }
 }
