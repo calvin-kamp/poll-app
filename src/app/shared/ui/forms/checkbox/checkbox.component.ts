088d46ae1c2e335efbe78b02a8c975522a3fdffb
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-01-23 16:28:30 +0100

    feat: update checkbox-component

--- src/app/shared/ui/forms/checkbox/checkbox.component.ts
+++ src/app/shared/ui/forms/checkbox/checkbox.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'ui-checkbox',
-    imports: [],
+    imports: [ReactiveFormsModule],
     templateUrl: './checkbox.component.html',
     styleUrl: './checkbox.component.scss',
 })
 
 export class CheckboxComponent {
+    label = input.required<string>();
+    name = input.required<string>();
+    errorMessage = input<string>('');
 
+    control = input<FormControl<string | null>>(new FormControl<string | null>(null));
 }
