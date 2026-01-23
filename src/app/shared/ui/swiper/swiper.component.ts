af9dab95a12475fc0e3d3d350d258e467b24deaf
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-01-23 01:28:09 +0100

    feat: rename swiper-component

---
+++ src/app/shared/ui/swiper/swiper.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ui-swiper',
+    imports: [],
+    templateUrl: './swiper.component.html',
+    styleUrl: './swiper.component.scss',
+})
+
+export class SwiperComponent {
+
+}
