5b056ca25e1dd7fbbafe5e988e158dfeb5c31095
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-01-26 17:25:52 +0100

    feat: add radio-component

--- src/app/shared/ui/forms/radio/radio.component.ts
+++ src/app/shared/ui/forms/radio/radio.component.ts
@@ -1,18 +1,85 @@
-import { Component, input } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Component, computed, inject, input, signal } from '@angular/core';
+import { RADIO_GROUP, RadioGroupApi } from '../radio-group/radio-group.token';
 
 @Component({
     selector: 'ui-radio',
-    imports: [ReactiveFormsModule],
     templateUrl: './radio.component.html',
     styleUrl: './radio.component.scss',
 })
 
 export class RadioComponent {
     label = input.required<string>();
-    name = input.required<string>();
-    value = input.required<string>();
-    errorMessage = input<string>('');
 
-    control = input<FormControl<string | null>>(new FormControl<string | null>(null));
+    name = input<string>('');
+    value = input.required<string>();
+
+    private radioGroup: RadioGroupApi | null = inject(RADIO_GROUP, { optional: true });
+
+    isReadySignal = signal(true);
+
+    isGroupMode = computed(() => {
+        return !!this.radioGroup;
+    });
+
+    get resolvedName(): string {
+        const explicitName = this.name();
+
+        if (explicitName) {
+            return explicitName;
+        }
+
+        if (this.isGroupMode()) {
+            return this.radioGroup!.getGroupName();
+        }
+
+        return '';
+    }
+
+    get checked(): boolean {
+        if (!this.isGroupMode()) {
+            return false;
+        }
+
+        return this.radioGroup!.getSelectedValue() === this.value();
+    }
+
+    get disabled(): boolean {
+        if (!this.isGroupMode()) {
+            return false;
+        }
+
+        return this.radioGroup!.isDisabled();
+    }
+
+    get isInvalid(): boolean {
+        if (!this.isGroupMode()) {
+            return false;
+        }
+
+        return this.radioGroup!.isInvalid();
+    }
+
+    get describedById(): string | null {
+        if (!this.isGroupMode()) {
+            return null;
+        }
+
+        return this.radioGroup!.getDescribedById();
+    }
+
+    onSelect(): void {
+        if (!this.isGroupMode()) {
+            return;
+        }
+
+        this.radioGroup!.setSelectedValue(this.value());
+    }
+
+    onBlur(): void {
+        if (!this.isGroupMode()) {
+            return;
+        }
+
+        this.radioGroup!.notifyTouched();
+    }
 }
